2bc28b654e6829d8f0416a434b1532e965df5dd2
Guilherme Henrique de Araújo Santos <guilhermeher10@gmail.com>
2021-06-19 12:32:11 -0300

    Criando classes blockchain

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-if __name__=='__main__':
-    pass
+from .SimpleMiner import SimpleMiner
+from .WorldOfMiners import WorldOfMiners
+
+if __name__=="__main__":
+    world = WorldOfMiners([1,2,3,4,5,6,7])
+    for w in range(100000):
+        w.step()
