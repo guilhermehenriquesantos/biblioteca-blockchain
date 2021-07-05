580449b329cc7e6e484e53cc2cbc4e665294954c
Guilherme Henrique de Araújo Santos <guilhermeher10@gmail.com>
2021-07-05 17:42:11 -0300

    Deletando arquivos que nao serao usados no momento

--- main.py
+++
@@ -1,7 +0,0 @@
-from .SimpleMiner import SimpleMiner
-from .WorldOfMiners import WorldOfMiners
-
-if __name__=="__main__":
-    world = WorldOfMiners([1,2,3,4,5,6,7])
-    for w in range(100000):
-        w.step()
