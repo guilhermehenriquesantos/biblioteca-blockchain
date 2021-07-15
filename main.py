17853ef4f018730c7a667b4853358da5c4e614f5
Guilherme Henrique de Araújo Santos <guilhermeher10@gmail.com>
2021-07-15 16:29:40 -0300

    Escolhendo minerador
    Criando funçoes mais especificas para a escolha do minerador

--- main.py
+++ main.py
@@ -5,30 +5,39 @@ from minerador import Minerador
 MINERADORES = {}
 
 
-if __name__ == '__main__':
-    maior_poder = 0
-    poder_mundial = 0
-
-    # Criando o array dos mineradores
+def cria_dicionario_mineradores():
     for identificador in range(1, 51):
         candidato_minerador = Minerador(identificador, random.randint(1, 100))
         MINERADORES[candidato_minerador.identificador] = candidato_minerador
 
-    # Olhando o poder mundial
-    # Escolhendo o maior poder entre os mineradores
-    for identificador, minerador in MINERADORES.items():
-        poder_mundial = poder_mundial + minerador.poder_mineracao
 
+def poder_mundial():
+    poder_mundial = 0
+    for minerador in MINERADORES.values():
+        poder_mundial = poder_mundial + minerador.poder_mineracao
+    print("\nO PODER DE MINERAÇÃO DO MUNDO É:", poder_mundial, "\n")
+
+
+def maior_poder_mineracao():
+    maior_poder = 0
+    for minerador in MINERADORES.values():
         if(minerador.poder_mineracao > maior_poder):
             maior_poder = minerador.poder_mineracao
 
-    print("\nO PODER DE MINERAÇÃO DO MUNDO É:", poder_mundial, "\n")
-    print("MINERADOR(ES) MAIS PODEROSO(S) DO MUNDO:")
+    return maior_poder
 
-    # Definindo os mineradores com maior poder de mineração
+
+def minerador_mais_poderoso():
+    print("MINERADOR(ES) MAIS PODEROSO(S) DO MUNDO:")
     for identificador, minerador in MINERADORES.items():
-        if(minerador.poder_mineracao == maior_poder):
+        if(minerador.poder_mineracao == maior_poder_mineracao()):
             print("-------------------------------------")
             print("Minerador: " + str(identificador) +
                   "\t|\tPoder: " + str(minerador.poder_mineracao))
             print("-------------------------------------")
+
+
+if __name__ == '__main__':
+    cria_dicionario_mineradores()
+    poder_mundial()
+    minerador_mais_poderoso()
