c783765b4743277de5d0d613db0aaec9d661c1c5
Guilherme Henrique de Araújo Santos <guilhermeher10@gmail.com>
2021-07-19 22:13:05 -0300

    Mudança na escolha do minerador
    Agora o minerador que vai minerar e escolhido por meio de uma loteria, que prioriza os mineradores mais poderosos

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from minerador import Minerador
 
 
 MINERADORES = {}
-PROBABILIDADE = random.randint(0, 100)
 
 
 def cria_dicionario_mineradores():
@@ -13,38 +12,45 @@ def cria_dicionario_mineradores():
         candidato_minerador = Minerador(identificador, random.randint(1, 100))
         MINERADORES[candidato_minerador.identificador] = candidato_minerador
 
+    return MINERADORES
+
 
 def descobre_poder_mundial():
     poder_mundial = 0
     for minerador in MINERADORES.values():
         poder_mundial = poder_mundial + minerador.poder_mineracao
-    print("\nO PODER DE MINERAÇÃO DO MUNDO É:", poder_mundial, "\n")
+    # print("\nO PODER DE MINERAÇÃO DO MUNDO É:", poder_mundial, "\n")
+
+    return poder_mundial
 
 
-def define_maior_poder_mineracao():
-    maior_poder = 0
+def escolher_minerador():
+    loteria = random.randint(1, 10*descobre_poder_mundial())
+    acumulado = 0
+
     for minerador in MINERADORES.values():
-        if(minerador.poder_mineracao > maior_poder):
-            maior_poder = minerador.poder_mineracao
+        acumulado = acumulado + minerador.poder_mineracao
 
-    return maior_poder
+        if (loteria < acumulado and minerador.tipo_minerador == "Forte"):
+            print("O minerador {} de poder {} caracterizado como um minerador do tipo {} está minerando\n".format(
+                minerador.identificador, minerador.poder_mineracao, minerador.tipo_minerador))
+        elif (minerador.tipo_minerador == "Mediano" and loteria < (acumulado - 1000)):
+            print("O minerador {} de poder {} caracterizado como um minerador do tipo {} está minerando\n".format(
+                minerador.identificador, minerador.poder_mineracao, minerador.tipo_minerador))
+        elif (minerador.tipo_minerador == "Fraco" and loteria < (acumulado - 1500)):
+            print("O minerador {} de poder {} caracterizado como um minerador do tipo {} está minerando\n".format(
+                minerador.identificador, minerador.poder_mineracao, minerador.tipo_minerador))
 
-
-def escolhe_minerador_mais_poderoso():
-    print("MINERADOR(ES) MAIS PODEROSO(S) DO MUNDO:")
-    for identificador, minerador in MINERADORES.items():
-        if(minerador.poder_mineracao == define_maior_poder_mineracao()):
-            print("-------------------------------------")
-            print("Minerador: " + str(identificador) +
-                  "\t|\tPoder: " + str(minerador.poder_mineracao))
-            print("-------------------------------------")
+    if (loteria > acumulado):
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print("\nNenhum minerador foi capaz de minerar\n")
 
 
 if __name__ == '__main__':
-    os.system('cls' if os.name == 'nt' else 'clear')
-    if (PROBABILIDADE > 10):
+    try:
+        os.system('cls' if os.name == 'nt' else 'clear')
         cria_dicionario_mineradores()
-        descobre_poder_mundial()
-        escolhe_minerador_mais_poderoso()
-    else:
-        print("\nNão foi possível ocorrer mineração!\n")
+
+        escolher_minerador()
+    except Exception as error:
+        print(error)
