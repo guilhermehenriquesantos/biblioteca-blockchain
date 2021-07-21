3bf95a5303fa4fb3bfc14538ebcc944b32652e1a
Guilherme Henrique de Araújo Santos <guilhermeher10@gmail.com>
2021-07-21 17:12:06 -0300

    Alterando construtor
    Retirando os tipos de mineradores para que eles sejam influenciados apenas pelo seu poder computacional para minerar

--- minerador.py
+++ minerador.py
@@ -3,20 +3,10 @@ import random
 
 class Minerador:
 
-    def __init__(self, identificador, poder_mineracao, tipo_minerador=None):
+    def __init__(self, identificador, poder_mineracao):
         self.identificador = identificador
         self.poder_mineracao = poder_mineracao
-        
-        if (poder_mineracao <= 20):
-            tipo_minerador = "Fraco"
-            self.tipo_minerador = tipo_minerador
-        elif (poder_mineracao > 20 and poder_mineracao <= 60):
-            tipo_minerador = "Mediano"
-            self.tipo_minerador = tipo_minerador
-        elif (poder_mineracao > 60 and poder_mineracao <= 100):
-            tipo_minerador = "Forte"
-            self.tipo_minerador = tipo_minerador
 
     def __str__(self):
-        return "{} é um minerador do tipo {} com poder de mineração de {}".format(self.identificador, self.tipo_minerador, self.poder_mineracao)
+        return "{} é um minerador com poder de mineração ({})".format(self.identificador, self.poder_mineracao)
     
