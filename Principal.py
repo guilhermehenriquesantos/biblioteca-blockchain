07de0ea593442ce49cd60d8d41ddfdf96d34f117
Guilherme Henrique de Araujo Santos <guilhermeher10@gmail.com>
2022-03-23 21:02:04 -0300

    Atualizando classe principal para execuçao

--- Principal.py
+++ Principal.py
@@ -1,34 +1,18 @@
 from os import error
 
 from conveniencias.Funcoes import *
-from TiposExecucoes import *
+from ExemploExecucao import *
 
 
 '''
 * Execução da aplicação
-* Objetivo: chamar os tipos de execuções existentes
+* Objetivo: Chamar o exemplo de execução
 *
 '''
 if __name__ == '__main__':
     try:
         limpar_tela()
-        escolha_usuario = menu_execucoes()
-
-        if (escolha_usuario == '1'):
-            executar_mineracao_explicativa()
-
-        elif (escolha_usuario == '2'):
-            executar_mineracao_baseada_realidade()
-
-        elif (escolha_usuario == '0'):
-            limpar_tela()
-            print('Saindo da aplicação...')
-            exit()
-
-        else:
-            limpar_tela()
-            print('Escolha uma opção válida, fechando aplicação...')
-            exit()
+        executar_mineracao_baseada_realidade()
 
     except Exception as error:
         print('Ocorreu o erro: {}'.format(error))
