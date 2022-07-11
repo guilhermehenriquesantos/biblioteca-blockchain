10e7737d64aaba7988e819ea43bf8a3585463648
Guilherme Henrique de Araujo Santos <guilhermeher10@gmail.com>
2022-07-10 23:16:36 -0300

    Prova de trabalho e consenso realizados

--- Bloco.py
+++ Bloco.py
@@ -1,10 +1,10 @@
 class Bloco:
-    def __init__(self, numero, dados, nonce=None, hash_anterior=None, hash_proprio=None):
+    def __init__(self, numero, dados, hash_anterior=None, nonce=None, hash_proprio=None):
         self.numero = numero
         self.dados = dados
-        self.nonce = nonce
         self.hash_anterior = hash_anterior
+        self.nonce = nonce
         self.hash_proprio = hash_proprio
 
     def __str__(self):
-        return 'Numero: {}\nDados: {}\nNonce: {}\nHash anterior: {}\nHash próprio: {}'.format(self.numero, self.dados, self.nonce, self.hash_anterior, self.hash_proprio)
+        return 'Numero: {}\nDados: {}\nHash anterior: {}\nNonce: {}\nHash próprio: {}'.format(self.numero, self.dados, self.nonce, self.hash_anterior, self.hash_proprio)
